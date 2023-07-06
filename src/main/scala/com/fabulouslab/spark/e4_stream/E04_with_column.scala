4bf47a2d9e5c6c6eb2fa65095c578c5f9703abd9
lapixx25 <129520380+lapixx25@users.noreply.github.com>
2023-07-06 13:09:39 +0200

    Update E04_with_column.scala

--- src/main/scala/com/fabulouslab/spark/e4_stream/E04_with_column.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E04_with_column.scala
@@ -8,14 +8,43 @@ object E04_with_column {
 
   def main(args: Array[String]) {
 
-    /**
-      * La fonction withColumn permet de créer un nouveau dataframe en ajoutant une nouvelle colonne,
-      * elle est souvent associée à des fonctions dans le package org.apache.spark.sql.functions.
-      * En utilisant withColumn:
-      *   - Enrichir le stream en ajoutant 2 colonnes :
-      *      - La date de traitement de l'event
-      *      - Le temps passé sur la page en second
-      * */
+   import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{col, current_timestamp, expr, unix_timestamp}
+
+object PageviewsEnrichmentApp {
+  def main(args: Array[String]): Unit = {
+    // Création de la session Spark
+    val spark = SparkSession.builder()
+      .appName("PageviewsEnrichmentApp")
+      .getOrCreate()
+
+    // Lecture du stream de données à partir du topic Kafka
+    val pageviewsStream = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "pageviews")
+      .load()
+
+    // Conversion des données de la colonne "value" en chaîne de caractères
+    val pageviewsDF = pageviewsStream
+      .selectExpr("CAST(value AS STRING)")
+      .select("value")
+
+    // Transformation des données en ajoutant les colonnes de date et de temps passé sur la page
+    val enrichedDF = pageviewsDF.withColumn("processing_date", current_timestamp())
+      .withColumn("time_spent_seconds", expr("unix_timestamp(processing_date) - unix_timestamp(value.timestamp)"))
+
+    // Affichage du DataFrame enrichi en continu dans la console
+    val query = enrichedDF.writeStream
+      .outputMode("append")
+      .format("console")
+      .start()
+
+    // Démarrage de la lecture en continu
+    query.awaitTermination()
+  }
+}
+
 
 
   }
