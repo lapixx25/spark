a97d4b7ce464601f5fdae370bcdc01cc0d25feed
lapixx25 <129520380+lapixx25@users.noreply.github.com>
2023-07-06 13:05:27 +0200

    Update E03_filter.scala

--- src/main/scala/com/fabulouslab/spark/e4_stream/E03_filter.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E03_filter.scala
@@ -10,14 +10,54 @@ object E03_filter {
   case class View(viewtime: Long, userid: String, pageid: String)
   def main(args: Array[String]) {
 
-    /**
-      *   Toujours sur le stream du topic pageviews :
-      *   - Construire un stream qui contient que les events sur la page 3
-      *   - Construire un stream qui contient que les events sur le user 3
-      *   - Construire un stream qui contient que les events sur le user 3 avec un temps de visite supérieur à 2 secondes
-      *   - Réinjecter ces streams dans un topic Kafka
-      * */
-    
+
+import java.util.Properties;
+
+public class PageviewsStreamProcessor {
+    public static void main(String[] args) {
+        // Configuration du Kafka Streams
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "pageviews-stream-processor");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+
+        // Création du flux builder Kafka Streams
+        StreamsBuilder builder = new StreamsBuilder();
+
+        // Flux original à partir du topic "pageviews"
+        KStream<String, String> pageviewsStream = builder.stream("pageviews");
+
+        // Construction d'un flux qui contient uniquement les événements sur la page 3
+        KStream<String, String> page3Stream = pageviewsStream.filter((key, value) -> {
+            // Ajoutez ici votre logique de filtrage pour les événements sur la page 3
+            // Exemple : return getPageId(value).equals("3");
+        });
+
+        // Construction d'un flux qui contient uniquement les événements sur le user 3
+        KStream<String, String> user3Stream = pageviewsStream.filter((key, value) -> {
+             return getUserId(value).equals("3");
+        });
+
+        // Construction d'un flux qui contient uniquement les événements sur le user 3 avec un temps de visite supérieur à 2 secondes
+        KStream<String, String> user3LongVisitStream = pageviewsStream.filter((key, value) -> {
+             return getUserId(value).equals("3") && getVisitDuration(value) > 2;
+        });
+
+        // Réinjection des flux filtrés dans les topics Kafka correspondants
+        page3Stream.to("page3-events", Produced.with(page3Stream.keySerde(), page3Stream.valueSerde()));
+        user3Stream.to("user3-events", Produced.with(user3Stream.keySerde(), user3Stream.valueSerde()));
+        user3LongVisitStream.to("user3-long-visit-events", Produced.with(user3LongVisitStream.keySerde(), user3LongVisitStream.valueSerde()));
+
+        // Création et démarrage du Kafka Streams
+        KafkaStreams streams = new KafkaStreams(builder.build(), props);
+        streams.start();
+
+        // Arrêt propre du Kafka Streams lors de l'arrêt de l'application
+        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+    }
+
+
+}
+
 
   }
 
