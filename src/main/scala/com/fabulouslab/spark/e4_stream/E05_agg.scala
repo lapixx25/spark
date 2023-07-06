b5a239840593f0e00128ce282f1bb984af73cfed
lapixx25 <129520380+lapixx25@users.noreply.github.com>
2023-07-06 13:11:37 +0200

    Update E05_agg.scala

--- src/main/scala/com/fabulouslab/spark/e4_stream/E05_agg.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E05_agg.scala
@@ -11,12 +11,68 @@ import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType
 object E05_agg {
 
   def main(args: Array[String]) {
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{avg, max, expr}
 
-    /**
-      *   - Compter le temps moyen passé sur une page,
-      *   - Compter le temps max passé sur une page,
-      *   - Compter en une seule fois le temps moyen et max passé sur une page.
-      * */
+object PageviewsAggregationApp {
+  def main(args: Array[String]): Unit = {
+    // Création de la session Spark
+    val spark = SparkSession.builder()
+      .appName("PageviewsAggregationApp")
+      .getOrCreate()
+
+    // Lecture du stream de données à partir du topic Kafka
+    val pageviewsStream = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("subscribe", "pageviews")
+      .load()
+
+    // Conversion des données de la colonne "value" en chaîne de caractères
+    val pageviewsDF = pageviewsStream
+      .selectExpr("CAST(value AS STRING)")
+      .selectExpr("CAST(value AS STRUCT<userId: STRING, pageId: STRING, timestamp: STRING>)")
+
+    // Transformation des données pour calculer le temps moyen passé sur une page
+    val avgTimeDF = pageviewsDF.withColumn("timestamp", expr("CAST(timestamp AS TIMESTAMP)"))
+      .groupBy("pageId")
+      .agg(avg(expr("UNIX_TIMESTAMP() - UNIX_TIMESTAMP(timestamp)")).alias("average_time"))
+
+    // Transformation des données pour calculer le temps maximum passé sur une page
+    val maxTimeDF = pageviewsDF.withColumn("timestamp", expr("CAST(timestamp AS TIMESTAMP)"))
+      .groupBy("pageId")
+      .agg(max(expr("UNIX_TIMESTAMP() - UNIX_TIMESTAMP(timestamp)")).alias("maximum_time"))
+
+    // Transformation des données pour calculer à la fois le temps moyen et le temps maximum passé sur une page
+    val avgMaxTimeDF = pageviewsDF.withColumn("timestamp", expr("CAST(timestamp AS TIMESTAMP)"))
+      .groupBy("pageId")
+      .agg(
+        avg(expr("UNIX_TIMESTAMP() - UNIX_TIMESTAMP(timestamp)")).alias("average_time"),
+        max(expr("UNIX_TIMESTAMP() - UNIX_TIMESTAMP(timestamp)")).alias("maximum_time")
+      )
+
+    // Affichage des résultats en continu dans la console
+    val query1 = avgTimeDF.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    val query2 = maxTimeDF.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    val query3 = avgMaxTimeDF.writeStream
+      .outputMode("complete")
+      .format("console")
+      .start()
+
+    // Démarrage de la lecture en continu
+    query1.awaitTermination()
+    query2.awaitTermination()
+    query3.awaitTermination()
+  }
+}
 
   }
 
