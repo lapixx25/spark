6fbf4c5b933d31c0e45401fc8e772bd07e66e45b
lapixx25 <129520380+lapixx25@users.noreply.github.com>
2023-07-06 12:17:40 +0200

    Update E02_json.scala

--- src/main/scala/com/fabulouslab/spark/e4_stream/E02_json.scala
+++ src/main/scala/com/fabulouslab/spark/e4_stream/E02_json.scala
@@ -9,11 +9,53 @@ object E02_json {
 
   def main(args: Array[String]) {
 
-    /**
-      *   Toujours sur le flux du topic pageviews :
-      *   - Déserialiser la value json en Struct
-      *   - Construire un dataframe depuis les attributs du Struct
-      * */
+object PageviewsStreamingApp {
+  def main(args: Array[String]): Unit = {
+    // Création de la session Spark
+    val spark = SparkSession.builder()
+      .appName("PageviewsStreamingApp")
+      .getOrCreate()
+
+    // Définition du schéma de la structure (Struct)
+    val schema = StructType(Seq(
+      StructField("userId", StringType),
+      StructField("pageId", StringType),
+      StructField("timestamp", StringType)
+    ))
+
+    // Configuration du streaming Kafka
+    val kafkaParams = Map(
+      "kafka.bootstrap.servers" -> "localhost:9092",
+      "subscribe" -> "pageviews",
+      "startingOffsets" -> "latest"
+    )
+
+    // Lecture des données du topic Kafka
+    val kafkaStream = spark.readStream
+      .format("kafka")
+      .options(kafkaParams)
+      .load()
+
+    // Extraction de la valeur (payload) du message Kafka
+    val valueStream = kafkaStream
+      .selectExpr("CAST(value AS STRING)")
+      .as[String]
+
+    // Conversion de la valeur JSON en DataFrame avec le schéma défini
+    val pageviewsDF = valueStream
+      .select(from_json(col("value"), schema).as("data"))
+      .select("data.*")
+
+    // Affichage du DataFrame en continu dans la console
+    val query: StreamingQuery = pageviewsDF.writeStream
+      .outputMode(OutputMode.Append())
+      .format("console")
+      .start()
+
+    // Démarrage de la lecture en continu
+    query.awaitTermination()
+  }
+}
 
   }
 
